30d23ad37df0f72a8a50ded375fde4cd20aa7d8b
cihanozbkk <90063224+cihanozbkk@users.noreply.github.com>
2024-08-09 14:22:07 +0300

    Add files via upload

---
+++ input_main.cpp
@@ -0,0 +1,5 @@
+//Search for:
+		else if (p->type == REFINE_TYPE_SCROLL || p->type == REFINE_TYPE_HYUNIRON || p->type == REFINE_TYPE_MUSIN || p->type == REFINE_TYPE_BDRAGON )
+	//Replace with:
+		else if (p->type == REFINE_TYPE_SCROLL || p->type == REFINE_TYPE_HYUNIRON || p->type == REFINE_TYPE_MUSIN || p->type == REFINE_TYPE_BDRAGON || p->type == REFINE_TYPE_RITUALS_SCROLL)
+	
